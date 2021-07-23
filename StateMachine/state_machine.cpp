883defdcbf1864466df2aebf0cb3bed4d03bde36
itsfeas <amf.fiaz@gmail.com>
2021-07-22 21:29:11 -0600

    added the udp module. still needs quite a bit of fixes.

--- StateMachine/state_machine.cpp
+++ StateMachine/state_machine.cpp
@@ -2,15 +2,16 @@
 #include <ChibiOS_ARM.h>
 #include "CANBus.h"
 #include "circular_buffer.h"
+#include <Ethernet2.h>
 
-#define PACKET_LENGTH 8
+#define PACKET_LENGTH 34
 #define ACCELERATION_IDX 2
 #define POSITION_IDX 6
 #define VELOCITY_IDX 10
 #define BATTERY_VOLTAGE_IDX 14
 #define N_MODULES 4
 
-//config script made with help of
+//flexcan config script made with help of
 //http://eet.etec.wwu.edu/KurtTaylor/project/docs/Bibleography/MCU/ConnSoftDocs/Kinetis%20SDK%20v2.0.0%20API%20Reference%20Manual/group__flexcan__driver.html
 flexcan_config_t flexcanConfig;
 
@@ -23,6 +24,22 @@ CircularBuffer<CAN_message_t, 10> in_buff, out_buff;
 uint16_t wait_time = 40; // 40 ms
 uint8_t CAN_id = 0;
 
+EthernetUDP Udp;
+
+uint32_t tube_length = 125000;
+double run_length = 30.0;
+uint8_t team_id = 0;
+
+double seconds = 0;
+unsigned long wait_time = 400; // 400 ms
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress local_ip(192, 168, 2, 177);
+IPAddress remote_ip(192, 168, 2, 30);
+
+unsigned int local_port = 3000;
+unsigned int remote_port = 3000;
+
 static struct States
 {
     uint32_t pod_state = Status::SafeToApproach;
@@ -32,6 +49,7 @@ static struct States
     double position = 0;
     double velocity = 0;
     double acceleration = 0;
+    bool should_launch = true;
 } pod_context;
 
 enum Status
@@ -76,22 +94,61 @@ uint8_t get_status()
     return 0;
 }
 
-// TODO: implement this function
-uint32_t get_position()
+double get_position(double seconds, double run_length, uint32_t tube_length)
 {
-    return 2334566;
+    return (1 - 1 * cos((double)seconds * PI / (double)run_length)) / 2.0 * tube_length;
 }
 
-// TODO: implement this function
-uint32_t get_velocity()
+double get_velocity(double seconds, double run_length, uint32_t tube_length)
 {
-    return 13454634;
+    return (get_position(seconds, run_length, tube_length) - get_position(seconds - 0.1, run_length, tube_length)) * 10;
 }
 
-// TODO: implement this function
-uint32_t get_acceleration()
+double get_acceleration(double seconds, double run_length, uint32_t tube_length)
 {
-    return 435468763;
+    return (get_velocity(seconds, run_length, tube_length) - get_velocity(seconds - 0.1, run_length, tube_length)) * 10;
+}
+
+uint8_t *build_packet(uint8_t team_id, uint8_t status, int32_t acceleration, int32_t position, int32_t velocity)
+{
+    static uint8_t packet[PACKET_LENGTH];
+    packet[0] = team_id;
+    packet[1] = status;
+
+    for (int i = ACCELERATION_IDX + 3; i >= ACCELERATION_IDX; i--)
+    {
+        packet[i] = acceleration & 0xFF;
+        acceleration = acceleration >> 8;
+    }
+
+    for (int i = POSITION_IDX + 3; i >= POSITION_IDX; i--)
+    {
+        packet[i] = position & 0xFF;
+        position = position >> 8;
+    }
+
+    for (int i = VELOCITY_IDX + 3; i >= VELOCITY_IDX; i--)
+    {
+        packet[i] = velocity & 0xFF;
+        velocity = velocity >> 8;
+    }
+
+    for (int i = BATTERY_VOLTAGE_IDX; i < PACKET_LENGTH; i++)
+    {
+        packet[i] = 0;
+    }
+
+    return packet;
+}
+
+void print_packet(uint8_t *packet)
+{
+    for (int i = 0; i < PACKET_LENGTH; i++)
+    {
+        Serial.print(packet[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.println();
 }
 
 uint8_t *fault_msg(uint8_t CAN_id, uint8_t fault_type)
@@ -206,7 +263,65 @@ static THD_FUNCTION(Thread2, arg)
     chRegSetThreadName("telemetry_module");
     while (1)
     {
-        //add udp thread details here
+        unsigned long start_time = millis();
+        uint32_t status = pod_context.pod_state;
+        if (status == Status::SafeToApproach && should_launch)
+        {
+            if (seconds > 10)
+            {
+                status = Status::ReadyToLaunch;
+                seconds = 0;
+                should_launch = false;
+            }
+        }
+        else if (status == Status::ReadyToLaunch)
+        {
+            if (seconds > 5)
+            {
+                status = Status::Launching;
+                seconds = 0;
+            }
+        }
+        else if (status == Status::Launching)
+        {
+            position = get_position(seconds, run_length, tube_length);
+            velocity = get_velocity(seconds, run_length, tube_length);
+            acceleration = get_acceleration(seconds, run_length, tube_length);
+
+            if (acceleration < 0)
+            {
+                status = Status::Braking;
+            }
+        }
+        else if (status == Status::Braking)
+        {
+            position = get_position(seconds, run_length, tube_length);
+            velocity = get_velocity(seconds, run_length, tube_length);
+            acceleration = get_acceleration(seconds, run_length, tube_length);
+
+            if (seconds >= run_length)
+            {
+                status = Status::SafeToApproach;
+            }
+        }
+        else if (status == Status::SafeToApproach)
+        {
+            position = tube_length;
+            velocity = 0;
+            acceleration = 0;
+        }
+
+        uint8_t *packet = build_packet(team_id, status, (int32_t)acceleration, (int32_t)position, (int32_t)velocity);
+        print_packet(packet);
+        Udp.beginPacket(remote_ip, remote_port);
+        Udp.write(packet, (size_t)PACKET_LENGTH);
+        Udp.endPacket();
+
+        unsigned long end_time = millis();
+        delay(wait_time - (end_time - start_time));
+        seconds += ((double)wait_time / (double)1000);
+
+
         chThdYield();
     }
 }
