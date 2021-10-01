d6b31ab84d5d123582870389568f98044bda90b4
itsfeas <amf.fiaz@gmail.com>
2021-10-01 15:06:34 -0600

    fixed prob with numbering

--- MotorController/messages.h
+++ MotorController/messages.h
@@ -7,8 +7,14 @@ uint8_t[11][7] snd_msg_list = {};
 
 //arr[0] is the length of the msg
 //arr[1-n] are the msg bytes
-uint8_t[7] arr = {0x3, 0x3D, 0xE2, 0x00};
+uint8_t[4] arr = {0x3, 0x3D, 0xE2, 0x00};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
-    rcv_msg_list[0][i] = arr[i];
+    snd_msg_list[0][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x3D, 0xE2, 0x00};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[0][i] = arr[i];
 }
