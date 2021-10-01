7c247080232a1959fbf086741bcea4978e412c3b
itsfeas <amf.fiaz@gmail.com>
2021-10-01 15:11:43 -0600

    added new msgs

--- MotorController/messages.h
+++ MotorController/messages.h
@@ -7,7 +7,16 @@ uint8_t[11][7] snd_msg_list = {};
 
 //arr[0] is the length of the msg
 //arr[1-n] are the msg bytes
+
+//change the 0 in snd_msg_list[0][i] = arr[i]; to the msg num
+//0xFF is a placeholder for values we will not send
 uint8_t[4] arr = {0x3, 0x3D, 0xE2, 0x00};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[0][i] = arr[i];
+}
+
+uint8_t[3] arr = {0x3, 0x51, 0xE2, 0x00};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
     snd_msg_list[0][i] = arr[i];
