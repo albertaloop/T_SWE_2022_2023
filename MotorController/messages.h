dff3272b6016e7962d0f66061e07d13be57e3524
Kevin-was-here <Xuyang4@ualberta.ca>
2021-10-01 15:19:43 -0600

    added all transmit messages

--- MotorController/messages.h
+++ MotorController/messages.h
@@ -3,25 +3,76 @@
 //This is our list of messages
 
 uint8_t[3][7] rcv_msg_list = {};
-uint8_t[11][7] snd_msg_list = {};
+uint8_t[10][7] snd_msg_list = {};
 
 //arr[0] is the length of the msg
 //arr[1-n] are the msg bytes
 
 //change the 0 in snd_msg_list[0][i] = arr[i]; to the msg num
 //0xFF is a placeholder for values we will not send
-uint8_t[4] arr = {0x3, 0x3D, 0xE2, 0x00};
+
+//Transmission messages
+int8_t[4] arr = {0x3, 0x3D, 0xE2, 0x00};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
     snd_msg_list[0][i] = arr[i];
 }
 
-uint8_t[3] arr = {0x3, 0x51, 0xE2, 0x00};
+uint8_t[4] arr = {0x3, 0x51, 0x04, 0x00};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
-    snd_msg_list[0][i] = arr[i];
+    snd_msg_list[1][i] = arr[i];
 }
 
+uint8_t[4] arr = {0x3, 0x3D, 0xE8, 0x00};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[2][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x51, 0x00, 0x00};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[3][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x35, 0xF4, 0x01};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[4][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0xED, 0xE8, 0x03};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[5][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x31, 0xD4, 0x03};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[6][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x3D, 0x30, 0x64};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[7][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x31, 0xA4, 0x7F};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[8][i] = arr[i];
+}
+
+uint8_t[4] arr = {0x3, 0x31, 0x00, 0x00};
+for (uint8_t i = 0; i < arr[0]; i++)
+{
+    snd_msg_list[9][i] = arr[i];
+}
+
+// Recieve messages
 uint8_t[5] arr = {0x4, 0xE2, 0x01, 0x00, 0x00};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
@@ -34,7 +85,7 @@ for (uint8_t i = 0; i < arr[0]; i++)
     rcv_msg_list[1][i] = arr[i];
 }
 
-uint8_t[5] arr = {0x4, 0x30, 0xff, 0xff, 0xff};
+uint8_t[5] arr = {0x4, 0x30, 0xFF, 0xFF, 0xFF};
 for (uint8_t i = 0; i < arr[0]; i++)
 {
     rcv_msg_list[2][i] = arr[i];
