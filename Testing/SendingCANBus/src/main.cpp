3397ad6b1e54b93f201ae6f31db10ad7c508d5ac
Kevin_was_here <xuyang4@ualberta.ca>
2022-01-24 19:53:10 -0700

    working gps code

---
+++ Testing/SendingCANBus/src/main.cpp
@@ -0,0 +1,153 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <FlexCAN_T4.h>
+
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+
+FlexCAN_T4<CAN0, RX_SIZE_256, TX_SIZE_16> Can0;
+
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
+  }
+
+  /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.println(mag, DEC);
+}
+
+void canSniff(const CAN_message_t &msg)
+{
+  Serial.print("MB ");
+  Serial.print(msg.mb);
+  Serial.print("  OVERRUN: ");
+  Serial.print(msg.flags.overrun);
+  Serial.print("  LEN: ");
+  Serial.print(msg.len);
+  Serial.print(" EXT: ");
+  Serial.print(msg.flags.extended);
+  Serial.print(" TS: ");
+  Serial.print(msg.timestamp);
+  Serial.print(" ID: ");
+  Serial.print(msg.id, HEX);
+  Serial.print(" Buffer: ");
+  for (uint8_t i = 0; i < msg.len; i++)
+  {
+    Serial.print(msg.buf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(14, OUTPUT);
+  digitalWrite(14, LOW);
+  Serial.println("Orientation Sensor Raw Data Test");
+
+  Can0.begin();
+  Can0.setBaudRate(500000);
+  Can0.setMaxMB(16);
+  // Can0.enableFIFO();
+  // Can0.onReceive(canSniff);
+  // Can0.mailboxStatus();
+
+  Serial.println("");
+
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    // while (1) {
+      
+    // };
+  }
+
+  delay(1000);
+
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+
+  bno.setExtCrystalUse(true);
+
+  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+  displayCalStatus();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+  Serial.print("X: ");
+  Serial.print(euler.x());
+  Serial.print(" Y: ");
+  Serial.print(euler.y());
+  Serial.print(" Z: ");
+  Serial.print(euler.z());
+  Serial.print("\n");
+  delay(100);
+
+  if ((abs(euler.x()) + abs(euler.y()) + abs(euler.z()))>0)
+  {
+    digitalWrite(14, HIGH);
+    // delay(250);
+  }
+  digitalWrite(14, LOW);
+
+  CAN_message_t msg;
+  msg.id = 0x1;
+  msg.len = 0x3;
+  msg.buf[0] = 100*euler.x();
+  msg.buf[1] = 100*euler.y();
+  msg.buf[2] = 100*euler.z();
+  Can0.write(msg);
+
+  Serial.print("MB ");
+  Serial.print(msg.mb);
+  Serial.print(" OVERRUN: ");
+  Serial.print(msg.flags.overrun);
+  Serial.print(" LEN: ");
+  Serial.print(msg.len);
+  Serial.print(" EXT: ");
+  Serial.print(msg.flags.extended);
+  Serial.print(" TS: ");
+  Serial.print(msg.timestamp);
+  Serial.print(" ID: ");
+  Serial.print(msg.id, HEX);
+  Serial.print(" Buffer: ");
+  for (uint8_t i = 0; i < msg.len; i++)
+  {
+    Serial.print(msg.buf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println("\n");
+}
