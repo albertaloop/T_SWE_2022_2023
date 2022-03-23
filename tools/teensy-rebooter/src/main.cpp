cf87106ebbee7d914187de1c7f887c48d6811f24
mdbardwell <bardwell@ualberta.ca>
2022-03-22 23:30:52 -0400

    rebooter: set pins as INPUT by default.
    also update gitignore

--- tools/teensy-rebooter/src/main.cpp
+++ tools/teensy-rebooter/src/main.cpp
@@ -1,3 +1,9 @@
+/* Teensy Rebooter Code
+ *
+ * Released under MIT license
+ * Author mike.d.bardwell@pm.me
+ */
+
 #include <Arduino.h>
 
 uint8_t ASCII_OFFSET = 48; // ie. 0 + 48 = 48 which is Zero in ASCII. 9 + 48 = 57 which is Nine in ASCII
@@ -7,6 +13,9 @@ uint8_t PROGRAMMING_TIME_SEC = 2;
 
 uint8_t programming_pin = 0;
 
+/* Stalls the program until the user inputs a valid pin number.
+ * Valid pin numbers are between 0-9.
+ */
 uint8_t get_low_pin()
 {
   int b = 0;
@@ -21,16 +30,25 @@ uint8_t get_low_pin()
 
 void setup() {
   for (int i = PROGRAMMING_PIN_MIN; i <= PROGRAMMING_PIN_MAX; ++i)
-    pinMode(i, OUTPUT_OPENDRAIN); // Open Drain because we only care about driving to ground
-                                  // https://open4tech.com/open-drain-output-vs-push-pull-output/
+  {
+    /* We have to set all of the pins to INPUT, or else the pins
+     * will go low when the rebooter Teensy starts, putting the
+     * Teensies we want to program into HalfKey state prematurely.
+     */
+    pinMode(i, INPUT_PULLUP);
+  }
   while(!Serial); // wait for Serial port
   Serial.println("Welcome to the Teensy Rebooter program. Press any key to start");
 }
 
 void loop() {
-  if (Serial.available()) { // press any key to restart program
+  if (Serial.available()) {
     Serial.println("Welcome to the Teensy Rebooter program.");
-    Serial.print("Tell us which pin to set low [1-10]: ");
+    Serial.print("Tell us which pin to set low [");
+      Serial.print(PROGRAMMING_PIN_MIN);
+        Serial.print("-");
+          Serial.print(PROGRAMMING_PIN_MAX);
+            Serial.print("]: ");
     programming_pin = get_low_pin();
     Serial.println(programming_pin);
     Serial.print("Pulling pin ");
@@ -38,9 +56,17 @@ void loop() {
       Serial.print(" low for ");
       Serial.print(PROGRAMMING_TIME_SEC);
         Serial.println(" seconds.");
+    /* Here we get creative. We set the programming pin to OUTPUT.
+     * Then, we set it LOW as is required to put the Teensy we want
+     * to program into HalfKey mode. We then wait a few seconds and then
+     * return the pin to INPUT mode. The Teensy we want to program will
+     * stay in HalfKey mode until we program it or power cycle the unit.
+     */
+    pinMode(programming_pin, OUTPUT);
     digitalWrite(programming_pin, LOW);
     delay(PROGRAMMING_TIME_SEC*1000);
     digitalWrite(programming_pin, HIGH);
+    pinMode(programming_pin, INPUT_PULLUP);
     Serial.println("Finished programming. Press any key to retry");
   }
 }
