a0fcd8e01aef4118c8c1eff60be263c14857637b
mdbardwell <bardwell@ualberta.ca>
2022-04-17 19:04:22 -0400

    pre-commit: init clang-format file

--- Firmware/canbus_one_teensy_two_nodes/src/main.cpp
+++ Firmware/canbus_one_teensy_two_nodes/src/main.cpp
@@ -1,8 +1,11 @@
+/*
+ * Copyright 2022 AlbertaLoop
+ */
 #include <FlexCAN_T4.h>
 #include <StensTimer.h>
 
-#include "node_stubs.hpp"
-#include "utils.hpp"
+#include "../include/node_stubs.hpp"
+#include "../include/utils.hpp"
 
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1;
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> can2;
@@ -21,9 +24,9 @@ struct bms _bms;
 StensTimer *bmsTimer = NULL;
 const int TICK_ACTION = 1;
 
-void prepare_bms_message(struct bms &_bms, CAN_message_t &msg) {
-  int_to_bytes2(_bms.current, msg.buf);
-  int_to_bytes2(_bms.voltage, &msg.buf[4]);
+void prepare_bms_message(const struct bms &_bms, CAN_message_t *msg) {
+  int_to_bytes2(_bms.current, msg->buf);
+  int_to_bytes2(_bms.voltage, &msg->buf[4]);
 }
 
 void timerCallback(Timer *timer) {
@@ -48,7 +51,7 @@ void setup(void) {
 
   bmsTimer = StensTimer::getInstance();
   bmsTimer->setStaticCallback(timerCallback);
-  bmsTimer->setInterval(TICK_ACTION, 1000); // Timer* myFirstTimer =
+  bmsTimer->setInterval(TICK_ACTION, 1000);  // Timer* myFirstTimer =
 }
 
 void print_buffer(const CAN_message_t &msg) {
