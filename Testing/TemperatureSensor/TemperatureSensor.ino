1c58d6f47c19412f6f1a00c93d0cf0d373e7f545
Kevin_was_here <xuyang4@ualberta.ca>
2022-04-30 13:07:29 -0600

    updated to use if else

--- Testing/TemperatureSensor/TemperatureSensor.ino
+++ Testing/TemperatureSensor/TemperatureSensor.ino
@@ -2,26 +2,25 @@
 //For tests change the sensor variable to responding number
 //Max6675 is 1
 //Tmp36 is 2
-#define sensor 2
+const int sensor = 1;
 
 #include <Wire.h>
-
 const int min_room_temp = 10;
 const int max_room_temp = 30;
 
 // Sample Arduino MAX6675 Arduino Sketch
 
-#if sensor == 1
+if(sensor == 1){
   #include "max6675.h"
   int ktcSO = 0;
   int ktcCS = 31;
   int ktcCLK = 32;
   MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
-#elif sensor == 2
+}else if (sensor == 2){
   int sensePin = A9;
   int sensorInput;
   double temp;
-
+}
 
 void setup(void)
 {
@@ -38,14 +37,14 @@ void loop(void) {
 //Roomtemp test returns true if test is data fetched is with roomtemp, false otherwise
 bool test_room_temp(void){
   //reads for 10 data points
-  #if sensor == 1
+  if (sensor == 1){
     for (size_t i = 0; i < 10; i++){
       if(!(min_room_temp < ktc.readCelsius() < max_room_temp)){
         Serial.println("Test Room Temperature failed!");
         return false;
       }
     }
-  #elif sensor == 2
+  }else if(sensor == 2){
     sensorInput = analogRead(sensePin);
     temp = (double)sensorInput / 1024;
     temp = (((temp*5)-0.5)*100); //TODO: check if this is calculated in C
@@ -55,7 +54,7 @@ bool test_room_temp(void){
         return false;
       }
     }
-    
+  }
   //true if roomtemp test passed
   return true;
 }
