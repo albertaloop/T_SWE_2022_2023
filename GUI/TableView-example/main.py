03ac90d1741a088834eaf71c971581a164903995
iann y <iyurychu@ualberta.ca>
2022-09-12 11:09:03 -0600

    Added UDPComms folder to 'testing'

---
+++ GUI/TableView-example/main.py
@@ -0,0 +1,12 @@
+class TableWindow(TableViewEx):
+    def __init__(self, window):
+        self.setupUi(window)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = TableWindow(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
