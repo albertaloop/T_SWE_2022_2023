a090c41403d53c2762cbe19c8add5e6bc6507724
NDCrocker <59457029+NDCrocker@users.noreply.github.com>
2022-10-29 14:05:17 -0600

    Adding initial python script to correct folder. Pushing to have access from laptop.

---
+++ Firmware/GPS_Module_Teensy3.6/parsing.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import numpy as np
+
+
+f = open('T_SWE_2022_2023/Testing/GPSTest/results1.txt', "r")   # open file from T_SWE_2022_2023 Directory
+
+line = f.readline() # read line from file
+
+
+
+# print(line)
+
+f.close()
