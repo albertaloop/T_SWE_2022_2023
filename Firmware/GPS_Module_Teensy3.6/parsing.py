bacb539140263d2454825b6f70d8a68370338841
NDCrocker <59457029+NDCrocker@users.noreply.github.com>
2022-11-12 13:16:48 -0700

    Changing path for right file read

--- Firmware/GPS_Module_Teensy3.6/parsing.py
+++ Firmware/GPS_Module_Teensy3.6/parsing.py
@@ -1,13 +1,12 @@
 import pandas as pd 
 import numpy as np
+import re 
 
 
-f = open('T_SWE_2022_2023/Testing/GPSTest/results1.txt', "r")   # open file from T_SWE_2022_2023 Directory
+f = open('T_SWE_2022_2023/Firmware/GPS_Module_Teensy3.6/testresults3.txt', "r")   # open file from T_SWE_2022_2023 Directory
 
 line = f.readline() # read line from file
 
-
-
-# print(line)
+print(line)
 
 f.close()
