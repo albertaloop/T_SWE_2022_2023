ccd62bd5f89673d4356ae5debde0f484d0f7e988
NDCrocker <59457029+NDCrocker@users.noreply.github.com>
2022-12-10 13:18:24 -0700

    Reorganized code structure based on new direction

--- Firmware/GPS_Module_Teensy3.6/parsing.py
+++ Firmware/GPS_Module_Teensy3.6/parsing.py
@@ -2,11 +2,30 @@ import pandas as pd
 import numpy as np
 import re 
 
+# Set up pattern recognition
+pointXRegex = re.compile()
+pointYRegex = re.compile()
+satRegex = re.compile() 
+distRegex = re.compile()
 
-f = open('T_SWE_2022_2023/Firmware/GPS_Module_Teensy3.6/testresults3.txt', "r")   # open file from T_SWE_2022_2023 Directory
+# Read the file 
+test3 = open('T_SWE_2022_2023/Firmware/GPS_Module_Teensy3.6/testresults3.txt', "r")   # open file from T_SWE_2022_2023 Directory
+test4 = open('T_SWE_2022_2023/Firmware/GPS_Module_Teensy3.6/testresults4.txt', "r")
 
-line = f.readline() # read line from file
 
-print(line)
 
-f.close()
+# Collect pattern strings 
+
+# Collect data from pattern strings
+
+# Plot the data
+
+
+# Old Crap 🤢
+# while line is not None:
+#     line = f.readline() # read line from file
+
+#     print(line)
+
+
+# f.close()
