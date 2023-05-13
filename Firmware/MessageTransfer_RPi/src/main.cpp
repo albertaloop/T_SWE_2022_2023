d6230f03d300fafc189a2cf5c22bba685f3f109a
Iyury1 <iyurychu@ualberta.ca>
2023-05-13 12:40:51 -0600

    UART loopback tested

--- Firmware/MessageTransfer_RPi/src/main.cpp
+++ Firmware/MessageTransfer_RPi/src/main.cpp
@@ -5,11 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "messages.h"
+#include <errno.h>
+#include <sys/ioctl.h>
+// #include "../include/messages.h"
 
 #define UART_TIMEOUT 100
 
 int serial_fd;
+int errnum;
+extern int errno;
 
 void pollUart() {
     struct pollfd src;
@@ -20,22 +24,15 @@ void pollUart() {
     int check = poll(&src, 1, -1);
 }
 
-static int read_uart(int uart_fd, void *data, size_t count) {
-    return 
-}
 
-static int write_uart(int uart_fd, void *data, size_t count) {
-    return write(uart_fd, data, count);
-}
-
-static int init_uart() {
+static void init_uart() {
     //https://raspberrypi.stackexchange.com/questions/57906/uart-interrupt-in-raspberry-pi-with-c-or-c
     // Open a file descriptor for the UART, use NDELAY for nonblocking file descriptor,
     // use O_NOCTTY to ensure this terminal device does not become the controlling terminal for
     // this process.
 
     // Could also use ttyS0
-    serial_fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+    serial_fd = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
     if(serial_fd < 0 || !isatty(serial_fd)) {
         perror("Problem opening termios fd");
         exit(1);
@@ -44,27 +41,31 @@ static int init_uart() {
     // Need to flush the port buffer before setting the UART settings
     usleep(250000);
     // TCIOFLUSH -> clear input buffer   
-    tcflush(serial_fd, TCIOFLUSH); 
+    // tcflush(serial_fd, TCIOFLUSH); 
 
     // Configure serial port for 8N1, no parity, no breaks
     // VMIN and VTIME are necessary for nonblocking reads
     struct termios serialSet;
     memset(&serialSet, 0, sizeof(serialSet));
-    serialSet.c_iflag = IGNBRK;
+    // tcgetattr(serial_fd, &serialSet);
+    // serialSet.c_iflag = IGNBRK;
+    serialSet.c_iflag = 0;
+    serialSet.c_oflag = 0;
+    serialSet.c_lflag = 0;
     // seerialSet.c_iflag = IGNPAR;
     // CS8 -> 8 bits, CREAD -> Enable receiver, CLOCAL -> Ignore modem status
     // Could also set BAUD rate here eg. B9600
     serialSet.c_cflag = CS8 | CREAD | CLOCAL;
-    memset(serialSet.c_cc, _POSIX_VDISABLE, NCCS);
-    serialSet.c_cc[VMIN] = 0;
-    serialSet.c_cc[VTIME] = 0;
+    // memset(serialSet.c_cc, _POSIX_VDISABLE, NCCS);
+    serialSet.c_cc[VMIN] = 1;
+    serialSet.c_cc[VTIME] = 5;
 
     // Set BAUD rate
-    speed_t baudRate = B115200;
+    speed_t baudRate = B9600;
     cfsetispeed(&serialSet, baudRate);
     cfsetospeed(&serialSet, baudRate);
+    // tcflush(serial_fd, TCIOFLUSH); 
 
-    tcgetattr(serial_fd, &serialSet);
 
     // TCSANOW -> apply the settings immediately
     if (tcsetattr(serial_fd, TCSANOW, &serialSet) == -1) {
@@ -80,15 +81,42 @@ int main() {
     src.fd = serial_fd;
     src.events = POLLIN;
     src.revents = 0;
-
+    char text[255];
+    // memset(text, 0, 255);
+    usleep(20000);
+    tcflush(serial_fd, TCIOFLUSH); 
+    usleep(20000);
+    ioctl(serial_fd, TCFLSH, 2);
+    read(serial_fd, text, 255);
+    printf("%s\n", text);
+    // memset(text, 0, 255);
+    read(serial_fd, text, 255);
+    printf("%s\n", text);
+    usleep(20000);
+    tcflush(serial_fd, TCIOFLUSH); 
     while(1) {
         // Send a message
-        write(serial_fd, "TLMTRY", strlen("TLMTRY"));
+        usleep(20000);
+        ioctl(serial_fd, TCFLSH, 2);
+        // tcflush(serial_fd, TCIOFLUSH); 
+        printf("Sending UART message\n");
+        usleep(20000);
+        int status = write(serial_fd, "TMTRY", strlen("TLMTRY"));
+        if(status < 0) {
+            printf("Write failure\n");
+        } else if (status == 0) {
+            printf("No write\n");
+            errnum = errno;
+            fprintf(stderr, "Value of errno: %d\n", errno);
+            perror("Error printed by perror");
+            fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+        }
         // Wait UART_TIMEOUT ms for acknowledgement
         // Resend message if timeout
         bool success = false;
         // https://stackoverflow.com/questions/67644306/how-can-we-determine-whether-a-socket-is-ready-to-read-write
         while(!success) {
+            printf("Checking for UART reply\n");
             if(poll(&src, 1, UART_TIMEOUT) < 0) {
                 perror("Poll error");
                 exit(1);
@@ -101,9 +129,23 @@ int main() {
                 }
             }
         }
-        char text[255];
-        read(serial_fd, text, 255);
-        printf("%.*s\n", text, 255);
+        usleep(2500000);
+        printf("Reading UART reply\n");
+        status = read(serial_fd, text, 255);
+        if(status < 0) {
+            printf("Read failure\n");
+            errnum = errno;
+            fprintf(stderr, "Value of errno: %d\n", errno);
+            perror("Error printed by perror");
+            fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+        } else if (status == 0) {
+            printf("No read\n");
+            errnum = errno;
+            fprintf(stderr, "Value of errno: %d\n", errno);
+            perror("Error printed by perror");
+            fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+        }
+        printf("%s\n", text);
         // Read message
         // delay
         usleep(250);
